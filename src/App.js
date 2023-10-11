b1ebfa7eee1a539d5215d628120038fe1a181af6
Wathmen777 <ooleksy@yandex.ru>
2023-10-11 20:52:28 +0400

    AddImages
    Добавлены изображения

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     {
       id:1,
       title:"Ноутбук Apple",
-      img:'',
+      img:'apple.jpg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'Apple',
       price:'1000'
@@ -16,7 +16,7 @@ export default function App() {
     {
       id:2,
       title:"Ноутбук Intel ",
-      img:'',
+      img:'intel.jpg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'Intel',
       price:'1000'
@@ -24,7 +24,7 @@ export default function App() {
     {
       id:3,
       title:"Ноутбук MSI",
-      img:'',
+      img:'msi.jpg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'MSI',
       price:'1000'
@@ -32,7 +32,7 @@ export default function App() {
     {
       id:4,
       title:"Ноутбук Lenovo",
-      img:'',
+      img:'lenovo.jpg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'Lenovo',
       price:'1000'
@@ -40,7 +40,7 @@ export default function App() {
     {
       id:5,
       title:"Жесткий диск Seagete",
-      img:'',
+      img:'seaget.jpg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'Seagete',
       price:'1000'
@@ -48,7 +48,7 @@ export default function App() {
     {
       id:6,
       title:"Жесткий диск Hitachi",
-      img:'',
+      img:'hitachi.jpg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'Hitachi',
       price:'1000'
@@ -56,7 +56,7 @@ export default function App() {
     {
       id:7,
       title:"Жесткий диск WD Blue",
-      img:'',
+      img:'wd.jpg',
       desc:'Жесткий диск WD Blue [WD10EZEX] станет надежным дополнением в настольном компьютере, обеспечивая надежность хранения различного контента и быстрый доступ к данным. Высокая производительность запоминающего устройства достигается за счет скорости вращения шпинделя 7200 об/мин и технологии NCQ.',
       category:'WD',
       price:'5000'
@@ -64,7 +64,7 @@ export default function App() {
     {
       id:8,
       title:"Оперативная память Atermiter ",
-      img:'',
+      img:'atermiter.jpeg',
       desc:'"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
       category:'Atermaiter',
       price:'1000'
@@ -72,7 +72,7 @@ export default function App() {
     {
       id:9,
       title:"Оперативная память Crucial Ballistix Black",
-      img:'',
+      img:'crucial.jpeg',
       desc:'16-гигабайтная оперативная память Crucial Ballistix Black [BL2K8G32C16U4B] будет полезна при сборке или модернизации игрового компьютера высокого класса. Вы получите в свое распоряжение два 8-гигабайтных модуля DDR4, оборудованных высокоэффективными радиаторами. Элементы теплоотвода необходимы, ведь «игровая» память часто используется в интенсивном режиме. Напряжение питания устройства повышено до 1.35 В.',
       category:'Ballistic',
       price:'5000'
@@ -80,7 +80,7 @@ export default function App() {
     {
       id:10,
       title:"Материнская плата MSI A320M-A PRO",
-      img:'',
+      img:'motherboard.jpg',
       desc:'Соответствующая форм-фактору Micro-ATX материнская плата MSI A320M-A PRO отличается компактностью: габаритные размеры устройства составляют 226x187 мм. Миниатюрность не мешает модели обеспечивать уровень функциональных возможностей, который удовлетворит потребности большого количества пользователей. ',
       category:'MSI',
       price:'6090'
