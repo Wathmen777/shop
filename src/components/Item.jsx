f8a929b451ac20a1c3adddf17394e217ae6fd4a9
Wathmen777 <ooleksy@yandex.ru>
2023-10-23 20:59:30 +0400

    Add data json

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function Item(props){
     return(
     <div className="item">
-        <img src={"./images/"+props.item2.img} alt=""/>
+        <div>
+        <img src={"./images/"+props.item2.img} onClick={()=>props.onShowItem(props.item2)} alt=""/>
         <h2>{props.item2.title}</h2>
         <p>{props.item2.desc}</p>
         <b>{props.item2.price}</b>
         <div className="add-to-cart" onClick={()=>props.onAdd(props.item2)}>+</div>
+        </div>
     </div>
     )
 }
--- src/components/ShowFullItem.jsx
+++ src/components/ShowFullItem.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-export default function ShowFullItem() {
+export default function ShowFullItem(props) {
     return(
-        <div>ShowItem</div>
+        <div className="full-item">
+            <div>
+            <img src={"./images/"+props.item.img} onClick={()=>props.onShowItem(props.item)} alt=""/>
+            <h2>{props.item.title}</h2>
+            <p>{props.item.desc}</p>
+            <b>{props.item.price}</b>
+            <div className="add-to-cart" onClick={()=>props.onAdd(props.item)}>+</div>
+            </div>
+        </div>
     );
 
 }
